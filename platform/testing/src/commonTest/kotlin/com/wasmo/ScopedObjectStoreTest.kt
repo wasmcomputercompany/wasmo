6428ba5e0f676db0836cdf52d6b653baee9e0482
Jesse Wilson <jesse@swank.ca>
2026-02-10 16:13:23 -0500

    test the unscoped store

--- platform/testing/src/commonTest/kotlin/com/wasmo/ScopedObjectStoreTest.kt
+++ platform/testing/src/commonTest/kotlin/com/wasmo/ScopedObjectStoreTest.kt
@@ -11,73 +11,73 @@ import okio.ByteString.Companion.encodeUtf8
 class ScopedObjectStoreTest : AbstractObjectStoreTest() {
   private val casino = "casino".encodeUtf8()
   private val subway = "subway".encodeUtf8()
-  override val store = FakeObjectStore()
-  private val pokerfaceStore = ScopedObjectStore(
+  private val unscopedStore = FakeObjectStore()
+  override val store = ScopedObjectStore(
     prefix = "shows/pokerface/",
-    delegate = store,
+    delegate = unscopedStore,
   )
 
   @Test
   fun scopedPutAndGet() = runTest {
-    store.put(PutObjectRequest("shows/pokerface/s1e1.mp4", casino))
-    pokerfaceStore.put(PutObjectRequest("s1e2.mp4", subway))
+    unscopedStore.put(PutObjectRequest("shows/pokerface/s1e1.mp4", casino))
+    store.put(PutObjectRequest("s1e2.mp4", subway))
+
+    assertThat(unscopedStore.get(GetObjectRequest("shows/pokerface/s1e1.mp4")))
+      .isEqualTo(GetObjectResponse(value = casino))
+    assertThat(unscopedStore.get(GetObjectRequest("shows/pokerface/s1e2.mp4")))
+      .isEqualTo(GetObjectResponse(value = subway))
+    assertThat(unscopedStore.get(GetObjectRequest("s1e1.mp4")))
+      .isEqualTo(GetObjectResponse(value = null))
+    assertThat(unscopedStore.get(GetObjectRequest("s1e2.mp4")))
+      .isEqualTo(GetObjectResponse(value = null))
 
     assertThat(store.get(GetObjectRequest("shows/pokerface/s1e1.mp4")))
-      .isEqualTo(GetObjectResponse(value = casino))
+      .isEqualTo(GetObjectResponse(value = null))
     assertThat(store.get(GetObjectRequest("shows/pokerface/s1e2.mp4")))
-      .isEqualTo(GetObjectResponse(value = subway))
+      .isEqualTo(GetObjectResponse(value = null))
     assertThat(store.get(GetObjectRequest("s1e1.mp4")))
-      .isEqualTo(GetObjectResponse(value = null))
-    assertThat(store.get(GetObjectRequest("s1e2.mp4")))
-      .isEqualTo(GetObjectResponse(value = null))
-
-    assertThat(pokerfaceStore.get(GetObjectRequest("shows/pokerface/s1e1.mp4")))
-      .isEqualTo(GetObjectResponse(value = null))
-    assertThat(pokerfaceStore.get(GetObjectRequest("shows/pokerface/s1e2.mp4")))
-      .isEqualTo(GetObjectResponse(value = null))
-    assertThat(pokerfaceStore.get(GetObjectRequest("s1e1.mp4")))
       .isEqualTo(GetObjectResponse(value = casino))
-    assertThat(pokerfaceStore.get(GetObjectRequest("s1e2.mp4")))
+    assertThat(store.get(GetObjectRequest("s1e2.mp4")))
       .isEqualTo(GetObjectResponse(value = subway))
   }
 
   @Test
   fun scopedList() = runTest {
-    store.put(PutObjectRequest("shows/pokerface/s1e1.mp4", casino))
-    pokerfaceStore.put(PutObjectRequest("s1e2.mp4", subway))
+    unscopedStore.put(PutObjectRequest("shows/pokerface/s1e1.mp4", casino))
+    store.put(PutObjectRequest("s1e2.mp4", subway))
 
+    assertThat(unscopedStore.list(ListObjectsRequest()).entries)
+      .containsExactly(
+        ListObjectsResponse.Object("shows/pokerface/s1e1.mp4", casino.etag, casino.size.toLong()),
+        ListObjectsResponse.Object("shows/pokerface/s1e2.mp4", subway.etag, subway.size.toLong()),
+      )
+    assertThat(unscopedStore.list(ListObjectsRequest(prefix = "shows/pokerface/")).entries)
+      .containsExactly(
+        ListObjectsResponse.Object("shows/pokerface/s1e1.mp4", casino.etag, casino.size.toLong()),
+        ListObjectsResponse.Object("shows/pokerface/s1e2.mp4", subway.etag, subway.size.toLong()),
+      )
     assertThat(store.list(ListObjectsRequest()).entries)
-      .containsExactly(
-        ListObjectsResponse.Object("shows/pokerface/s1e1.mp4", casino.etag, casino.size.toLong()),
-        ListObjectsResponse.Object("shows/pokerface/s1e2.mp4", subway.etag, subway.size.toLong()),
-      )
-    assertThat(store.list(ListObjectsRequest(prefix = "shows/pokerface/")).entries)
-      .containsExactly(
-        ListObjectsResponse.Object("shows/pokerface/s1e1.mp4", casino.etag, casino.size.toLong()),
-        ListObjectsResponse.Object("shows/pokerface/s1e2.mp4", subway.etag, subway.size.toLong()),
-      )
-    assertThat(pokerfaceStore.list(ListObjectsRequest()).entries)
       .containsExactly(
         ListObjectsResponse.Object("s1e1.mp4", casino.etag, casino.size.toLong()),
         ListObjectsResponse.Object("s1e2.mp4", subway.etag, subway.size.toLong()),
       )
-    assertThat(pokerfaceStore.list(ListObjectsRequest(prefix = "shows/pokerface/")).entries)
+    assertThat(store.list(ListObjectsRequest(prefix = "shows/pokerface/")).entries)
       .isEmpty()
   }
 
   @Test
   fun scopedDelete() = runTest {
-    store.put(PutObjectRequest("shows/pokerface/s1e1.mp4", casino))
-    store.put(PutObjectRequest("shows/pokerface/s1e2.mp4", subway))
+    unscopedStore.put(PutObjectRequest("shows/pokerface/s1e1.mp4", casino))
+    unscopedStore.put(PutObjectRequest("shows/pokerface/s1e2.mp4", subway))
 
-    pokerfaceStore.delete(DeleteObjectRequest("s1e1.mp4"))
-    pokerfaceStore.delete(DeleteObjectRequest("shows/pokerface/s1e2.mp4")) // Not deleted.
+    store.delete(DeleteObjectRequest("s1e1.mp4"))
+    store.delete(DeleteObjectRequest("shows/pokerface/s1e2.mp4")) // Not deleted.
 
-    assertThat(store.get(GetObjectRequest("shows/pokerface/s1e1.mp4")))
+    assertThat(unscopedStore.get(GetObjectRequest("shows/pokerface/s1e1.mp4")))
       .isEqualTo(GetObjectResponse(value = null))
-    assertThat(store.get(GetObjectRequest("shows/pokerface/s1e2.mp4")))
+    assertThat(unscopedStore.get(GetObjectRequest("shows/pokerface/s1e2.mp4")))
       .isEqualTo(GetObjectResponse(value = subway))
-    assertThat(store.list(ListObjectsRequest()).entries).containsExactly(
+    assertThat(unscopedStore.list(ListObjectsRequest()).entries).containsExactly(
       ListObjectsResponse.Object("shows/pokerface/s1e2.mp4", subway.etag, subway.size.toLong()),
     )
   }
