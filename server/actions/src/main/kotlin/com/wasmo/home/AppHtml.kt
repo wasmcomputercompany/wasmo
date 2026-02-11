8d560d562cd19440e14a94d20e279e654c46df61
Jesse Wilson <jesse@swank.ca>
2026-02-11 16:04:02 -0500

    Fix title

--- server/actions/src/main/kotlin/com/wasmo/home/AppHtml.kt
+++ server/actions/src/main/kotlin/com/wasmo/home/AppHtml.kt
@@ -31,7 +31,7 @@ class AppPage() : ResponseBody {
     writeHtml {
       head {
         meta(charset = "utf-8")
-        title("WASM COMPUTER")
+        title("Wasmo")
         meta(
           name = "viewport",
           content = "width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1",
