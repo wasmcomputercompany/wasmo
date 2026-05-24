5afbf8a71415da2860ad52a9525fcb23ebb54897
Tobias Thierer <github@tobias-thierer.de>
2026-05-24 20:19:34 +0200

    Don't create a ComputerAllocation for free computers.
    This is my preferred alternative over https://github.com/wasmcomputercompany/wasmo/pull/57
    
    It turns out that RealCallDataService.computerListSnapshot doesn't check
    for allocations, so this still works.
    
    Planned TODO for follow-up commit:
    
     - Computer DB table: add a column `payment_method VARCHAR(20)` with
       possible values `stripe` and `none` (for now); computers with
       `payment_method == none` don't need a ComputerAllocation.
    
    Optional TODOs for follow-up commits:
    
     - Edit docs/code/{subscriptions,system_model}.md to note that there can
       be free computers who do not need an allocation.
     - Consider renaming ComputerAllocation -> PaidComputerAllocation
       or ComputerPayment globally?

--- os/server/computers/real/src/jvmMain/kotlin/com/wasmo/computers/free/FreeCreateComputerSpecRpc.kt
+++ os/server/computers/real/src/jvmMain/kotlin/com/wasmo/computers/free/FreeCreateComputerSpecRpc.kt
@@ -6,19 +6,14 @@ import com.wasmo.api.CreateComputerSpecRequest
 import com.wasmo.api.CreateComputerSpecResponse
 import com.wasmo.computers.ComputerSpecStore
 import com.wasmo.computers.ComputerStore
-import com.wasmo.db.computers.insertComputerAllocation
 import com.wasmo.framework.Response
 import com.wasmo.framework.RpcAction
 import com.wasmo.framework.Url
-import com.wasmo.identifiers.StripeCustomerId
-import com.wasmo.payments.ComputerAllocationSnapshot
 import dev.zacsweers.metro.ClassKey
 import dev.zacsweers.metro.ContributesIntoMap
 import dev.zacsweers.metro.Inject
 import dev.zacsweers.metro.binding
 import kotlin.time.Clock
-import kotlin.time.Duration
-import kotlin.time.Duration.Companion.days
 import wasmo.sql.SqlDatabase
 import wasmox.sql.transaction
 
@@ -42,23 +37,6 @@ class FreeCreateComputerSpecRpc(
         computerSpecToken = request.computerSpecToken,
       )
       val computer = computerStore.initializeFromSpec(request.computerSpecToken)
-      val now = clock.now()
-
-      // TODO: Replace these fake values with something better; make the fields nullable?
-      val stripeCustomerId = StripeCustomerId(-1)
-      val stripeSubscriptinId = ""
-      val activeEnd = now.plus(36525.days) // approx. 100 years,
-
-      insertComputerAllocation(
-        created_at = now,
-        version = 1,
-        stripe_customer_id = stripeCustomerId,
-        stripe_subscription_id = stripeSubscriptinId,
-        computer_id = computer.id,
-        active_start = now,
-        active_end = activeEnd,
-      )
-
     }
     return Response(
       body = CreateComputerSpecResponse.Success,
