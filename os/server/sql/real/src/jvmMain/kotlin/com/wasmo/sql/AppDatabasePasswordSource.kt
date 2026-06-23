fceea6119acb8a3c9a67c52d1e8777fddca20895
Michael Pawliszyn <mpawliszyn@gmail.com>
2026-06-23 14:16:39 -0400

    Adding a hmac scheme to generate db passwords.

--- os/server/sql/real/src/jvmMain/kotlin/com/wasmo/sql/AppDatabasePasswordSource.kt
+++ os/server/sql/real/src/jvmMain/kotlin/com/wasmo/sql/AppDatabasePasswordSource.kt
@@ -1,25 +1,36 @@
 package com.wasmo.sql
 
+import com.wasmo.identifiers.AppSlug
+import com.wasmo.identifiers.ComputerSlug
 import com.wasmo.identifiers.DatabaseSlug
-import com.wasmo.identifiers.OsScope
+import com.wasmo.identifiers.InstalledAppScope
 import com.wasmo.identifiers.Secret
 import dev.zacsweers.metro.Inject
 import dev.zacsweers.metro.SingleIn
+import okio.ByteString.Companion.encodeUtf8
 
 /**
- * Currently we just run a scheme based on os password information and the app information.
+ * Currently we just run a scheme based on os password information and computer + app information.
  *
  * We deem this safe because if an attacker happens to obtain the os password then they would
  * already be able to impersonate anyone on that machine.
  */
 @Inject
-@SingleIn(OsScope::class)
+@SingleIn(InstalledAppScope::class)
 class AppDatabasePasswordSource(
   private val config: WasmoPostgresqlConfig,
+  private val computerSlug: ComputerSlug,
+  private val appSlug: AppSlug,
 ) {
   fun retrievePassword(databaseSlug: DatabaseSlug): Secret {
-    //TODO: Actually generate something here.
-    return Secret("app-password")
+    val passwordSource = computerSlug.value + DELIMITER + appSlug.value + DELIMITER + databaseSlug.value
+    val derivedPassword = passwordSource.encodeUtf8()
+      .hmacSha256(config.osPassword.rawUnredactedSecret.encodeUtf8())
+      .base64()
+    return Secret(derivedPassword)
   }
 
+  companion object {
+    const val DELIMITER = "\n"
+  }
 }
