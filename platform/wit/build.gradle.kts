c8f930559a5e1a1538e7a9ef50a75054f9ea345e
Jesse Wilson <jesse@wasmo.com>
2026-08-30 12:43:03 -0400

    Declare the ObjectStore API in .wit (#155)

--- platform/wit/build.gradle.kts
+++ platform/wit/build.gradle.kts
@@ -17,9 +17,13 @@ brevity {
   generateKotlin {
     worlds.add("wasmo:platform/wasmo")
     inputWitPackageDirectories.from(
+      File(project.projectDir, "src/wit/content-type"),
       File(project.projectDir, "src/wit/jobs"),
+      File(project.projectDir, "src/wit/json"),
+      File(project.projectDir, "src/wit/object-store"),
       File(project.projectDir, "src/wit/platform"),
       File(project.projectDir, "src/wit/sql"),
+      File(project.projectDir, "src/wit/uuid"),
       File(project.rootDir, "submodules/wasi-p2/preview2/cli"),
       File(project.rootDir, "submodules/wasi-p2/preview2/clocks"),
       File(project.rootDir, "submodules/wasi-p2/preview2/filesystem"),
